9b327438ac6b10d739db772ffec2ece3a6d99cc8
david <sp.david00@gmail.com>
2022-10-26 04:39:09 -0500

    fix: Refactor de elementos repetitivos

--- src/app/components/achievements-section/achievements-section.component.ts
+++ src/app/components/achievements-section/achievements-section.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./achievements-section.component.scss']
 })
 export class AchievementsSectionComponent implements OnInit {
-
+  listArchievements=[{
+    title:'10000+',
+    text:'Downloads per day',
+    imgUrl:'/assets/images/FeatureIcon1.png',
+  },{
+    title:'2 Million',
+    text:'Users',
+    imgUrl:'/assets/images/FeatureIcon2.png',
+  },{
+    title:'500+',
+    text:'Clients',
+    imgUrl:'/assets/images/FeatureIcon3.png',
+  },{
+    title:'140',
+    text:'Countries',
+    imgUrl:'/assets/images/FeatureIcon4.png',
+  }]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/features-section/features-section.component.ts
+++ src/app/components/features-section/features-section.component.ts
@@ -6,7 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./features-section.component.scss']
 })
 export class FeaturesSectionComponent implements OnInit {
-
+  listFeatures=[{
+    title:'Robust workflow',
+    text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
+    imgUrl:'/assets/images/FuatureIcon.png',
+  },{
+    title:'Flexibility',
+    text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
+    imgUrl:'/assets/images/FeatureIcon1.png',
+  },{
+    title:'User friendly',
+    text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
+    imgUrl:'/assets/images/FeatureIcon2.png',
+  },{
+    title:'Multiple layouts',
+    text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
+    imgUrl:'/assets/images/FeatureIcon3.png',
+  },{
+    title:'Better components',
+    text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
+    imgUrl:'/assets/images/FeatureIcon4.png',
+  },{
+    title:'Well organised',
+    text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
+    imgUrl:'/assets/images/FeatureIcon5.png',
+  }]
   constructor() { }
 
   ngOnInit(): void {
