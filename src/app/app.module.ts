d2791f53ed286d87dc4aabb61c350d9cb24ea8a5
david <sp.david00@gmail.com>
2022-10-26 04:16:20 -0500

    feat: Seccion de Archievements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {LayoutModule} from '@angular/cdk/layout';
 import { HeadSectionComponent } from './components/head-section/head-section.component';
 import { FeaturesSectionComponent } from './components/features-section/features-section.component';
+import { AchievementsSectionComponent } from './components/achievements-section/achievements-section.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { FeaturesSectionComponent } from './components/features-section/features
     LandingPageComponent,
     NavBarComponent,
     HeadSectionComponent,
-    FeaturesSectionComponent
+    FeaturesSectionComponent,
+    AchievementsSectionComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/achievements-section/achievements-section.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AchievementsSectionComponent } from './achievements-section.component';
+
+describe('AchievementsSectionComponent', () => {
+  let component: AchievementsSectionComponent;
+  let fixture: ComponentFixture<AchievementsSectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AchievementsSectionComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AchievementsSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
